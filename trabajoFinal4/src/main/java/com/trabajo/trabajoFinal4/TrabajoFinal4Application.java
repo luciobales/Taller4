5ca514e872559fbe4240e3e0a6354d655a1541bb
Lucius Bales <1+luciobales@users.noreply.github.com>
2018-07-18 13:33:36 -0300

    Falta agregar dependency JPA
    <dependency>
              <groupId>org.springframework.boot</groupId>
              <artifactId>spring-boot-starter-data-jpa</artifactId>
    </dependency>

---
+++ trabajoFinal4/src/main/java/com/trabajo/trabajoFinal4/TrabajoFinal4Application.java
@@ -0,0 +1,12 @@
+package com.trabajo.trabajoFinal4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFinal4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoFinal4Application.class, args);
+	}
+}
